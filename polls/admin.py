044c6e130c707c081d59b92abf9cfc86aca89777
Bakytzhan <bakytzhan.akzhol@ce.sdu.edu.kz>
2015-03-08 16:47:33 +0600

    Na4inau zanovo

--- polls/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from polls.models import Question
-# Register your models here.
-
-admin.site.register(Question)
-
-class QuestionAdmin(admin.ModelAdmin):
-	list_display = ('question_text', 'pub_date')
